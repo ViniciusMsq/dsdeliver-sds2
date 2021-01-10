3e93629f62765b3be6f3d66c09ca2dde6d68dc80
Vinicius Mesquini <viniciusm.gc@outlook.com>
2021-01-10 11:40:49 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,16 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import './Navbar/index';
 import Routes from './Rountes';
 
 function App() {
   return (
+    <>
     <Routes/>
+    <ToastContainer />
+    </>
   );
 }
 
